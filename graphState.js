7ebc64dc1109b5dad8576ea37b2d148b0274f6aa
CalvinDong <48747266+CalvinDong@users.noreply.github.com>
2021-03-04 11:47:55 +1100

    - drag working

--- graphState.js
+++ graphState.js
@@ -10,32 +10,10 @@ let data;
 data = dummyData;
 console.log(data)
 
-
-/*
-let p = d3.select("body")
-        .selectAll("p")
-        .data(data)
-        .text(dat => {return dat});
-*/
-
-/*
-var nodes = [{}, {}, {}, {}, {}]
-var links = [
-  {source: 0, target: 1},
-  {source: 0, target: 2},
-  {source: 0, target: 3},
-  {source: 1, target: 4},
-  {source: 3, target: 4},
-  {source: 3, target: 3},
-  {source: 4, target: 0},
-  {source: 4, target: 0}
-]
-*/
-
 var nodes = [];
 var links = [];
-ReadAdjMatrix(data)
 
+ReadAdjMatrix(data)
 
 const link = d3.select('svg')
   .selectAll('line')
@@ -43,78 +21,38 @@ const link = d3.select('svg')
   .join('line') // join adds a line for each edge in links (also lets you specify entry, exit update behaviour)
   .attr('stroke', "black")
 
-var simulation = d3.forceSimulation(nodes)
+const node = d3.select('svg')
+  .selectAll('cirlce')
+  .data(nodes)
+  .join('circle')
+  .attr('r', 8)
+
+const simulation = d3.forceSimulation(nodes)
   .force('charge', d3.forceManyBody().strength(-10))
   .force('center', d3.forceCenter(300 / 2, 300 / 2)) 
   .force('link', d3.forceLink().links(links)) // This is what creates the network
   .on('tick', ticked);
 
-var drag = d3.drag()
+const drag = d3.drag()
   .on('drag', dragged);
 
-d3.selectAll('circle').call(drag)
 
-function dragged(){
-  let current = d3.select(this);
-  current
-  .attr('cx', d3.event.x)
-  .attr('cy', d3.event.y)
-  .attr('r', 5);
+node.call(drag)
+
+function dragged(event, d){
+  d3.select(this).attr("cx", d.x = event.x).attr("cy", d.y = event.y);
 }
 
-function updateNodes(){ // Create nodes
-  var u = d3.select('svg')
-      .selectAll('circle')
-      .data(nodes) // bind node data to circle objects in DOM
-  
-    u.enter() // Adding the nodes that haven't been added yet
-      .append('circle')
-      .attr('r', 8)
-      .merge(u)
-      .attr('cx', function(d) {
-        //console.log(d.x)
-        return d.x
-      })
-      .attr('cy', function(d) {
-        return d.y
-      })
-  
-    u.exit().remove()
-}
-
-function updateLinks() { // Create links
-  /*var u = d3.select('svg')
-    .selectAll('line')
-    .data(links) // bind links data to line objects in DOM
-
-    u.enter() // Adding the lines that haven't been added yet
-      .append('line') 
-      .merge(u)
-      .attr('stroke', "black")
-      .attr('x1', function(d) {
-        return d.source.x
-      })
-      .attr('y1', function(d) {
-        return d.source.y
-      })
-      .attr('x2', function(d) {
-        return d.target.x
-      })
-      .attr('y2', function(d) {
-        return d.target.y
-      })
-
-  //u.exit().remove()*/
-  link
+function ticked() { // Update position of nodes and links for every simulation tick
+  link 
       .attr("x1", d => d.source.x)
       .attr("y1", d => d.source.y)
       .attr("x2", d => d.target.x)
       .attr("y2", d => d.target.y);
-}
 
-function ticked() {
-  updateLinks()
-  updateNodes()
+  node
+    .attr('cx', d => d.x)
+    .attr('cy', d => d.y)
 }
 
 function ReadAdjMatrix(matrix){
