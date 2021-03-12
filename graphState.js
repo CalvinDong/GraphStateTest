9b2984744be812ebf04b54836801d1e206a1fd6b
Calvin Dong <calvin.dong156@gmail.com>
2021-03-12 12:49:31 +1100

    - removing kind of works

--- graphState.js
+++ graphState.js
@@ -14,6 +14,9 @@ let nodes = [];
 let links = [];
 let circles;
 let text;
+let nodeAdd = false;
+let nodeRemove = false;
+let drag = true;
 
 ReadAdjMatrix(data)
 
@@ -51,13 +54,14 @@ const text = node.append('text')
 */
 
 
-var simulation = d3.forceSimulation(nodes)
+const simulation = d3.forceSimulation(nodes)
   .force('charge', d3.forceManyBody().strength(-0.5))
   .force('center', d3.forceCenter(300 / 2, 300 / 2)) 
   .force('link', d3.forceLink().links(links)) // This is what creates the network
   .on('tick', ticked);
 
 let svg = d3.select('svg')
+  .on("click", clickedSVG);
 
 let node = svg.selectAll('.node')
   .data(nodes) 
@@ -68,7 +72,8 @@ let node_enter = node.join('g')
       .on("start", dragstarted)
       .on('drag', dragged)
       .on('end', dragEnded)
-    );
+    )
+  .on("click", clickedNode);
 
 node_enter.append('circle')
   .attr('r', 8)
@@ -83,23 +88,35 @@ link = svg.selectAll('line')
   .join('line') // join adds a line for each edge in links (also lets you specify entry, exit update behaviour)
   .attr('stroke', "black")
 
+function clickedSVG(event, d){
+  if (nodeAdd && event.srcElement === svg._groups[0][0]){ // svg groups to prevent spawning on nodes
+    AddNode(event)
+  }
+}
+
+function clickedNode(event, d){
+  console.log(event)
+  console.log(d)
+  if (nodeRemove){
+    RemoveNode(d)
+  }
+
+}
+
 function dragstarted(d) {
-  //if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-  //d.fx = d.x;
-  //d.fy = d.y;
+
 }
 
 function dragged(event, d){
-  /*d3.select(this)
-    .attr("transform", d => `translate(${d.x = event.x}, ${d.y = event.y})`);*/ //For moving g elements
-  
   d3.select(this)
-    .attr("cx", d.x = event.x).attr("cy", d.y = event.y);
+    .attr("transform", d => `translate(${d.x = event.x}, ${d.y = event.y})`); //For moving g elements
+  
+  //d3.select(this)
+  //  .attr("cx", d.x = event.x).attr("cy", d.y = event.y);
 }
 
 function dragEnded(){
   simulation.alpha(1).restart();
-  //if (!d3.event.active) simulation.alphaTarget(0);
 }
 
 function ticked() { // Update position of nodes and links for every simulation tick
@@ -135,6 +152,12 @@ function DrawNodes(){
 
   node_enter = node.enter().append('g') // enter().append() pattern to draw one at a time, .join() for all data in array
     .attr('class', 'node')
+    .call(d3.drag()
+      .on("start", dragstarted)
+      .on('drag', dragged)
+      .on('end', dragEnded)
+    )
+  .on("click", clickedNode);
 
   node_enter.append('circle')
     .attr('r', 8)
@@ -150,14 +173,46 @@ function DrawNodes(){
     .attr('stroke', "black")
 }
 
-function AddNode(){
+function AddNode(event){
   console.log("adding")
   let tempNodes = [...nodes];
-  tempNodes.push({id: nodes.length})
+  tempNodes.push({id: nodes.length, x: event.x, y: event.y})
   nodes = tempNodes 
   DrawNodes()
 
   simulation.nodes(nodes)
   simulation.force("link").links(links);
   simulation.alpha(1).restart();
+}
+
+function RemoveNode(d){
+  console.log("removing")
+  nodes.splice(d.index, 1);
+  links = links.filter(function(n){
+    return (n.source != d && n.target != d)
+  })
+  console.log(links)
+  DrawNodes()
+
+  simulation.nodes(nodes)
+  simulation.force("link").links(links);
+  simulation.alpha(1).restart();
+}
+
+function AddNodeActive(){
+  nodeAdd = true;
+  nodeRemove = false;
+  drag = false;
+}
+
+function RemoveNodeActive(){
+  nodeAdd = false;
+  nodeRemove = true;
+  drag = false;
+}
+
+function DragActive(){
+  nodeAdd = false;
+  nodeRemove = false;
+  drag = true;
 }
